5a5fd1196424668b5de9be6c623d5662c853adc3
Jqu1nteroT <juanpabloquintero25@gmail.com>
2017-06-28 16:10:21 +0200

    serie 1

---
+++ exc01/index.php
@@ -0,0 +1,84 @@
+<?php
+<?php
+
+//Exc 01
+$nom = "Quintero";
+
+	echo $nom;
+
+echo "<hr>";
+
+//Exc 02
+
+$nom = "Quintero";
+$prenom = "Pablo";
+$age = 26;
+
+	echo "mon nom est $prenom $nom et j'ai $age";
+
+echo "<hr>";
+
+//Exc 03
+
+$km = 1;
+
+$km = 3;
+
+	echo $km;
+
+$km = 125;
+
+echo "<br>";
+
+	echo $km;
+
+echo "<hr>";
+
+//Exc 04
+
+$str = "chien";
+$int = 24;
+$fload = 3.45;
+$boo = true;
+
+	echo "$str<br> $int<br> $fload<br> $boo";
+
+echo "<hr>";
+
+//Exc 05
+
+$x = "";
+
+	echo "$x";
+
+$x = 5;
+
+	echo "$x";
+
+echo "<hr>";
+
+//Exc 06
+
+	echo "Bonjour " .$prenom.", comment vas tu?";
+
+echo "<hr>";
+
+//Exc 07
+
+	echo "Bonjour " . $nom ." ". $prenom . ", tu as " . $age . " ans.";
+
+echo "<hr>";
+
+//Exc 08
+
+	$x = 3 + 4;
+	$y = 5 * 20;
+	$z = 45 / 5;
+
+	echo "Premier: ".$x." Deuxieme: ".$y." Troisieme: ".$z;
+
+
+?>
+
+
+?>
